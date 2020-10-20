5bf4eb8c7f2cef4aa0c643cfcb6309c719b23b12
Exr0n <spotyie@gmail.com>
2020-10-19 18:41:55 -0700

    failed iterator attempt
    I'll work on it eventually, but first I need an mvp

--- src/engine/state_memhog.hpp
+++ src/engine/state_memhog.hpp
@@ -1,11 +1,22 @@
 #ifndef FOXSTR_STATE
 #define FOXSTR_STATE
 
+#include <iterator>
+
 #include "config.hpp"
 
 class State
 {
     State* _nxt[ALPHABET_SIZE];
+    // TODO: convert to iterator someday
+    //class iterator: public std::iterator<
+    //    // from https://en.cppreference.com/w/cpp/iterator/iterator
+    //                std::random_access_iterator_tag,
+    //                const State,
+    //                size_t,
+    //                const State*,
+    //                const State>
+    //{}
 public:
     void print();
 };
